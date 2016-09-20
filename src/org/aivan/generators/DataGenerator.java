9417a7996cb20a5905bbdd61be1a7b7a6f01bc33
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2016-09-20 23:32:57 +0200

    Add long generators, initial version

--- src/org/aivan/generators/DataGenerator.java
+++ src/org/aivan/generators/DataGenerator.java
@@ -1,7 +1,8 @@
 package org.aivan.generators;
 
 /**
- * Utility class for generating all sorts of data (ordered, random, string, numeric, etc.)
+ * Utility class for generating all sorts of data (ordered, random, string,
+ * numeric, etc.)
  * 
  * @author aivan
  *
@@ -9,12 +10,34 @@ package org.aivan.generators;
 public class DataGenerator {
 
 	/**
-	 * Generate an array of ints starting from 0 up to length-1, in an ascending ordered array
+	 * Generate an array of long-s starting from 0 up to length-1, in an
+	 * ascending ordered array
 	 * 
 	 * @param length
 	 * @return
 	 */
-	int[] generateAscendingOrderedIntArray(int length) {
-		return null;
+	public static long[] generateAscendingOrderedLongArray(int length) {
+		long[] result = new long[length];
+		for (int i = 0; i < length; i++) {
+			result[i] = i;
+		}
+		return result;
 	}
+	
+	public static long[] generateDescendingOrderedLongArray(int length) {
+		long[] result = new long[length];
+		for (int i = 0; i < length; i++) {
+			result[i] = length-i-1;
+		}
+		return result;
+	}
+	
+	public static long[] generateRandomLongArray(int length) {
+		long[] result = new long[length];
+		for (int i = 0; i < length; i++) {
+			result[i] = (long) (Math.random()+length);
+		}
+		return result;
+	}
+	
 }
--- test/org/aivan/generators/DataGeneratorTest.java
+++ test/org/aivan/generators/DataGeneratorTest.java
@@ -3,11 +3,61 @@ package org.aivan.generators;
 import org.junit.Assert;
 import org.junit.Test;
 
-
-
 public class DataGeneratorTest {
+	private static final int INT_ARRAY_LENGTH = 10;
+
 	@Test
-	public void dummyTest() {
-		Assert.assertTrue(true);
+	public void orderedLongLengthZeroTest() {
+		long[] longs = DataGenerator.generateAscendingOrderedLongArray(0);
+		Assert.assertNotNull("Generated array must not be null", longs);
+		Assert.assertEquals(longs.length, 0);
+	}
+
+	@Test
+	public void orderedLongTest() {
+		long[] longs = DataGenerator.generateAscendingOrderedLongArray(INT_ARRAY_LENGTH);
+		Assert.assertNotNull("Generated array must not be null", longs);
+		for (int i = 0; i < INT_ARRAY_LENGTH; i++) {
+			Assert.assertEquals("Ascending array of ints must contain elements equal to index", i, longs[i]);
+		}
+		Assert.assertEquals("First element is 0", longs[0], 0);
+		Assert.assertEquals("Last element is length-1", longs[INT_ARRAY_LENGTH - 1], INT_ARRAY_LENGTH - 1);
+		Assert.assertEquals("There should be exactly length elements", longs.length, INT_ARRAY_LENGTH);
+
+	}
+
+	@Test
+	public void orderedLongDescendingLengthZeroTest() {
+		long[] longs = DataGenerator.generateDescendingOrderedLongArray(0);
+		Assert.assertNotNull("Generated array must not be null", longs);
+		Assert.assertEquals(longs.length, 0);
+	}
+
+	@Test
+	public void orderedLongDescendingTest() {
+		long[] longs = DataGenerator.generateDescendingOrderedLongArray(INT_ARRAY_LENGTH);
+		Assert.assertNotNull("Generated array must not be null", longs);
+		for (int i = 0; i < INT_ARRAY_LENGTH; i++) {
+			Assert.assertEquals("Ascending array of ints must contain elements equal to index",
+					INT_ARRAY_LENGTH - i - 1, longs[i]);
+		}
+		Assert.assertEquals("First element is length-1", longs[0], INT_ARRAY_LENGTH - 1);
+		Assert.assertEquals("Last element is 0", longs[INT_ARRAY_LENGTH - 1], 0);
+		Assert.assertEquals("There should be exactly length elements", longs.length, INT_ARRAY_LENGTH);
+
+	}
+
+	@Test
+	public void randomLongLengthZeroTest() {
+		long[] longs = DataGenerator.generateRandomLongArray(0);
+		Assert.assertNotNull("Generated array must not be null", longs);
+		Assert.assertEquals(longs.length, 0);
+	}
+
+	@Test
+	public void randomLongTest() {
+		long[] longs = DataGenerator.generateRandomLongArray(INT_ARRAY_LENGTH);
+		Assert.assertNotNull("Generated array must not be null", longs);
+		Assert.assertEquals("There should be exactly length elements", longs.length, INT_ARRAY_LENGTH);
 	}
 }
